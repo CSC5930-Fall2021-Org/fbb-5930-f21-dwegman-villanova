aa1451dd6dcef91a3ed51124104a726e5c731a14
dwegman-villanova <dwegman@villanova.edu>
2021-09-05 18:41:18 -0400

    Update FooBarBaz.java

--- src/FooBarBaz.java
+++ src/FooBarBaz.java
@@ -1,4 +1,19 @@
 
 public class FooBarBaz {
-
+	public static void main(String[] args) 
+	{
+		int n = 50;
+		
+		for (int i=0;i<=n;i++)
+		{
+				if (i%3==0 && i%5==0 && i!=0)
+					System.out.print("foobar\n");
+				else if (i%3==0 && i!=0)
+					System.out.print("foo\n" );
+				else if(i%5==0 && i!=0)
+					System.out.print("baz\n"); 
+			else 
+				System.out.print(i + "\n");
+		}
+	}
 }
