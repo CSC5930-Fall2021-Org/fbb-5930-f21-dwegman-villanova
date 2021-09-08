ea0ebdc7ff6e33c7d53bd8aa3630aa12acd6e2b7
dwegman-villanova <dwegman@villanova.edu>
2021-09-07 21:19:12 -0400

    Update FooBarBaz.java

--- src/FooBarBaz.java
+++ src/FooBarBaz.java
@@ -9,61 +9,67 @@ public class FooBarBaz {
 		
 		System.out.println("\nEnter the input");	
 		String input = sc.nextLine();
-		input = input.replaceAll("\\s", "");
-		if (input.equalsIgnoreCase("stop"))
+		String trim = input.trim();
+		//String inputws = input.replaceAll("\\s", "");
+		if (trim.equalsIgnoreCase("stop"))
 		{
 			break;
 		}
-		int n = Integer.parseInt(input);
-		//while(input.equalsIgnoreCase("stop"))
-		//{
-		
-				//int n = 110;
-		
-		
-				if(n>=0)
-				{
-					for (int i=0;i<=n;i++)
+		try
+		{
+			int n = Integer.parseInt(trim);
+			//while(input.equalsIgnoreCase("stop"))
+			//{
+			
+					//int n = 110;
+					if(n>=0 && n<=2147483647)
 					{
-						if (i%3==0 && i%5==0 && i%7==0 && i!=0)
+						for (int i=0;i<=n;i++)
 						{
-							System.out.print("foobarbaz"+ "\n");
-						}
-						else if (i%3==0 && i%5==0 && i!=0)
-						{
-							System.out.print("foobar"+ "\n");
-						}
-						else if (i%3==0 && i%7==0 && i!=0)
-						{
-							System.out.print("foobaz"+ "\n");
-						}
-						else if (i%5==0 && i%7==0 && i!=0)
-						{
-							System.out.print("barbaz"+ "\n");
-						}
-						else if (i%3==0 && i!=0)
-						{
-							System.out.print("foo"+ "\n" );
-						}
-						else if(i%5==0 && i!=0)
-						{
-							System.out.print("bar"+ "\n");
-						}
-						else if(i%7==0 && i!=0)
-						{
-							System.out.print("baz"+ "\n");
-						}
-						else
-						{
-							//System.out.print("\n");
-							System.out.print(i+ "\n");
+							System.out.print(i + "=");
+							if (i%3==0 && i%5==0 && i%7==0 && i!=0)
+							{
+								System.out.print("foobarbaz"+ "\n");
+							}
+							else if (i%3==0 && i%5==0 && i!=0)
+							{
+								System.out.print("foobar"+ "\n");
+							}
+							else if (i%3==0 && i%7==0 && i!=0)
+							{
+								System.out.print("foobaz"+ "\n");
+							}
+							else if (i%5==0 && i%7==0 && i!=0)
+							{
+								System.out.print("barbaz"+ "\n");
+							}
+							else if (i%3==0 && i!=0)
+							{
+								System.out.print("foo"+ "\n" );
+							}
+							else if(i%5==0 && i!=0)
+							{
+								System.out.print("bar"+ "\n");
+							}
+							else if(i%7==0 && i!=0)
+							{
+								System.out.print("baz"+ "\n");
+							}
+							else
+							{
+								//System.out.print("\n");
+								System.out.print(i+ "\n");
+							}
 						}
 					}
-				}
-				else 
-					System.out.print("Invalid input\n");	
-				
+					else 
+						System.out.print(trim + "=" + "Invalid\n");	
+		}
+		catch(NumberFormatException e) 
+		{
+			   System.out.print(input + "=" + "Invalid\n");
+		}
 		}
 	}
-	}
-//}
+	
+}
