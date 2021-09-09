1d14fb9745abd0d90983fab7ec40f54dde136348
dwegman-villanova <dwegman@villanova.edu>
2021-09-08 21:43:52 -0400

    Update FooBarBaz.java

--- src/FooBarBaz.java
+++ src/FooBarBaz.java
@@ -1,22 +1,37 @@
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
 
 public class FooBarBaz {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception 
 	{
-		while(true)
+		if (args==null || args.length==0)
+		//if (args[0])
 		{
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("\nEnter the input");	
-		String input = sc.nextLine();
-		String trim = input.trim();
-		//String inputws = input.replaceAll("\\s", "");
-		if (trim.equalsIgnoreCase("stop"))
-		{
-			break;
+			System.out.println("Enter \"batch\" or \"console\" as argument.");
 		}
-		try
+		else
 		{
+			System.out.println("Hello " + args[0] + ".");
+		
+		if(args[0].equals("console"))
+		{
+			while(true)
+			{
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("\nEnter the input");	
+			String input = sc.nextLine();
+			String trim = input.trim();
+			//String inputws = input.replaceAll("\\s", "");
+			if (trim.equalsIgnoreCase("stop"))
+			{
+				break;
+			}
+			try
+			{
 			int n = Integer.parseInt(trim);
 			//while(input.equalsIgnoreCase("stop"))
 			//{
@@ -64,12 +79,104 @@ public class FooBarBaz {
 					}
 					else 
 						System.out.print(trim + "=" + "Invalid\n");	
+				}
+				catch(NumberFormatException e) 
+				{
+					   System.out.print(input + "=" + "Invalid\n");
+				}
+			}
 		}
-		catch(NumberFormatException e) 
+		
+			
+		//Path filepath = Paths.get("src/resources/fbb-input.txt");
+		if(args[0].equals("batch"))
 		{
-			   System.out.print(input + "=" + "Invalid\n");
+				
+			
+			String filename = "src/resources/fbb-input.txt";
+			Scanner sc2 = null;
+			
+			String outputfile = "src/resources/fbb-actual-output.txt";
+			PrintWriter writer = null;
+			
+			try {
+				sc2 = new Scanner(Paths.get(filename));
+				writer = new PrintWriter(outputfile);
+				while (sc2.hasNextLine())
+				{
+					String inputb = sc2.nextLine();
+					//System.out.println(line);
+					//writer.println(line);
+					String trimb = inputb.trim();
+					
+					try
+					{
+						int b = Integer.parseInt(trimb);
+						
+						if(b>=0 && b<=2147483647)
+						{
+							//for (int i=0;i<=n;i++)
+							//{
+								writer.print(inputb + "=");
+								if (b%3==0 && b%5==0 && b%7==0 && b!=0)
+								{
+									writer.print("foobarbaz"+ "\n");
+								}
+								else if (b%3==0 && b%5==0 && b!=0)
+								{
+									writer.print("foobar"+ "\n");
+								}
+								else if (b%3==0 && b%7==0 && b!=0)
+								{
+									writer.print("foobaz"+ "\n");
+								}
+								else if (b%5==0 && b%7==0 && b!=0)
+								{
+									writer.print("barbaz"+ "\n");
+								}
+								else if (b%3==0 && b!=0)
+								{
+									writer.print("foo"+ "\n" );
+								}
+								else if(b%5==0 && b!=0)
+								{
+									writer.print("bar"+ "\n");
+								}
+								else if(b%7==0 && b!=0)
+								{
+									writer.print("baz"+ "\n");
+								}
+								else
+								{
+									//System.out.print("\n");
+									writer.print(b + "\n");
+								}
+							//}
+						}
+						else 
+						{
+							writer.print(trimb + "=" + "Invalid\n");
+						}
+						
+					}
+					catch(NumberFormatException e)
+					{
+						writer.print(inputb + "=" + "Invalid\n");
+					}
+					
+				}
+				
+			}
+			catch(IOException e)
+			{
+				System.err.println("Unable to open file or write file.");
+			}
+			finally {
+				sc2.close();
+				writer.close();
+			}
 		}
-		}
+		
 	}
-	
+	}
 }
